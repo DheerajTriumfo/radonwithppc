70ac2d155e107bbe732c42aab12309c2a2c530f8
TRIUMFO\web1 <d.mkt@triumfo.de>
2025-08-20 17:39:12 +0530

    robots and siemap;

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,6 +28,19 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         {}
+        <Script
+          id="gtm-head"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WTBJPRN');
+            `,
+          }}
+        />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;800&display=swap" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@100..900&display=swap" />
@@ -35,6 +48,14 @@ export default function RootLayout({ children }) {
         
       </head>
       <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-WTBJPRN`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <Navigation/>
         {children}
         <SpeedInsights url="https://radonexhibitions.de/" />
