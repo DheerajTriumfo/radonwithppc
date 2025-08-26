dd641b40909d023bae655189c1f4afa796c9c9b3
TRIUMFO\web1 <d.mkt@triumfo.de>
2025-08-26 13:30:34 +0530

    for compression;

--- next.config.js
+++ next.config.js
@@ -3,4 +3,21 @@ module.exports = {
     domains: ['radonexhibition.com', 'radonllcapi.mobel.us'], // Add this line
   },
   trailingSlash: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // apply to all routes
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
 };
