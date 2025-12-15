7ee64d545870d9ca76e105f056eed12ca28e270f
TRIUMFO\web1 <d.mkt@triumfo.de>
2025-12-15 12:13:30 +0530

    sdfsd;

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,8 @@ import Myvideo from './home/video.js';
 import Link from "next/link";
 
 import "../styles/home.css";
+import Script from "next/script";
+
 export const metadata = {
   title : "Las Vegas Trade Show Booth Rentals | 750+Trade Booth Design",
    description : "Radon Exhibition LLC — Your Trusted Las Vegas Trade Show Booth Builder, offering custom booth designs and rental solutions in Las Vegas and across the USA.",
