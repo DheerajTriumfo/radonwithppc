83155b5d0b14d295abc761bddcfe0f54d3a36d01
TRIUMFO\web1 <d.mkt@triumfo.de>
2026-02-04 14:46:11 +0530

    dfsd;

---
+++ src/app/project-manager/page.js
@@ -0,0 +1,71 @@
+import styles from '../../styles/career-detail.css';
+import Image from "next/image";
+import Link from "next/link";
+export const metadata = {
+	title: 'Project Manager | Radon LLC',
+	description: '',
+	alternates: {
+	    canonical: 'https://radonexhibition.com/career/',
+	},
+}
+export default function Thankyou()
+{
+	return(
+		<>
+			<div className="careerbg">
+		    	<div className="container mx-auto">
+		    		<h1 className="maintitle">Project Manager</h1>
+		    	</div>
+		    </div>
+		    <section>
+	         <div className="job-details-sec">
+	            <div className="container">
+	               <div className="row">
+	                  <div className="col-lg-12">
+	                     <div className="career-inner">
+	                        <div className="container">
+	                           <div className="gray-box">
+	                              <div className="left">
+	                                 <div className="postiontitle">Project Manager</div>
+	                                 <div className="locationnel">Las Vegas, United States</div>
+	                                 <div className="fulltime">Full Time</div>
+	                              </div>
+	                              <div className="right">
+	                                 <div className="applynow"><Link href="/apply-now/" className="openapply">Apply Now</Link></div>
+	                              </div>
+	                           </div>
+	                           
+	                           <div className="job-title">Job Profile: Project Manager</div>
+	                           <ul>
+	                              <li>Supervise the quality and timely project delivery at Exhibitions and Tradeshow booth.</li>
+	                              <li>Read and work from generated production order details, sketches, plans and specifications</li>
+	                              <li>Experience in Exhibition/ Tradeshow booth building and manufacturing experience.</li>
+	                              <li>Communicate and work effectively within a team</li>
+	                              <li>Must provide professional instruction and communication for other coordinators (union workers, Customer Service department, electricians, etc)</li>
+	                              <li>Traveling and working at offsite locations required</li>
+	                              <li>Effectively works as part of a team while interfacing with all levels of staff and clientele</li>
+	                              <li>Works well in a fast-pace, rapidly changing work environment</li>
+	                              <li>Flexibility to work occasionally weekends and overtime during hectic, busy times with production deadlines looming Occasional travel to offsite installations.</li>
+	                              <li>Travel to offsite installations.</li>
+	                           </ul>
+	                           <div className="job-title">Other Duties:</div>
+	                           <ul>
+	                              <li>Maintain a clean and safe environment at all times.</li>
+	                              <li>Communicate with other Department personnel to ensure accurate client/booth information.</li>
+	                           </ul>
+	                           <div className="job-title">Qualifications:</div>
+	                           <ul>
+	                              <li>5 years+ proven experience in project management preferably in the Exhibitions and Tradeshow booth industry.</li>
+	                           </ul>
+	                           <p><strong>If interested please send your application to the address: <a href="https://web.archive.org/web/20250421081950/mailto:info@radonexhibition.com">info@radonexhibition.com</a></strong></p>
+	                           <div className="applynow"><Link href="/apply-now/" className="openapply">Apply Now</Link></div>
+	                        </div>
+	                     </div>
+	                  </div>
+	               </div>
+	            </div>
+	         </div>
+	      </section>
+		</>
+		);
+}
---
+++ src/app/site-supervisor/page.js
@@ -0,0 +1,60 @@
+import styles from '../../styles/career-detail.css';
+import Image from "next/image";
+import Link from "next/link";
+export const metadata = {
+	title: 'Sales Coordinator | Radon LLC',
+	description: '',
+	alternates: {
+	    canonical: 'https://radonexhibition.com/career/',
+	},
+}
+export default function Thankyou()
+{
+	return(
+		<>
+			<div className="careerbg">
+		    	<div className="container mx-auto">
+		    		<h1 className="maintitle">Production cum show site supervisor</h1>
+		    	</div>
+		    </div>
+		    <section>
+	         <div className="job-details-sec">
+	            <div className="container">
+	               <div className="row">
+	                  <div className="col-lg-12">
+	                     <div className="career-inner">
+	                        <div className="container">
+	                           <div className="gray-box">
+	                              <div className="left">
+	                                 <div className="postiontitle">Production cum show site supervisor</div>
+	                                 <div className="locationnel">Las Vegas, United States</div>
+	                                 <div className="fulltime">Full Time</div>
+	                              </div>
+	                              <div className="right">
+	                                 <div className="applynow"><Link href="/apply-now/" className="openapply">Apply Now</Link></div>
+	                              </div>
+	                           </div>
+	                           <div className="job-title">Key Responsibilities: </div>
+	                           <ul>
+	                              <li>•	To be able to pick and pack the BE MATRIX booth as per the design and drawings.</li>
+	                              <li>•	To be able to do all jobs connected with creating the mockup of the booths in the warehouse. </li>
+	                              <li>•	To Coordinate with designers, purchase, organize ordering of material purchases</li>
+	                              <li>•	Travel to different show sites and cross-country travel when required.</li>
+	                              <li>•	Supervise booth installation at the site and coordinate with all involved teams.</li>
+	                              <li>•	Coordinate with clients, venue officials, vendors, and internal teams during setup.</li>
+	                              <li>•	Manage last-minute changes, troubleshoot site challenges.</li>
+	                              <li>•	I&D coordination: Oversee dismantling and material return to warehouse post event.</li>
+	                              <li>•	Ensure the booth is completed on time and handed over to the client in good condition.</li>
+	                           </ul>
+	                           <p><strong>If interested please send your application to the address: <a href="https://web.archive.org/web/20250421081950/mailto:info@radonexhibition.com">info@radonexhibition.com</a></strong></p>
+	                           <div className="applynow"><Link href="/apply-now/" className="openapply">Apply Now</Link></div>
+	                        </div>
+	                     </div>
+	                  </div>
+	               </div>
+	            </div>
+	         </div>
+	      </section>
+		</>
+		);
+}
