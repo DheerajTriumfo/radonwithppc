58319c1833045b5b7de2f43fc02dffebc878dc4d
DheerajTriumfo <d.mkt@triumfo.de>
2026-09-16 19:46:51 +0530

    changes;

--- src/app/[locurl]/[detail]/page.js
+++ src/app/[locurl]/[detail]/page.js
@@ -220,6 +220,9 @@ export default async function BoothDetail(props) {
         <section>
           <div className="boothdetailbg">
             <div className="container">
+              <div className="widthsmall text-center mb-3">
+                <h2 className="maintitle mb-3">{boothdetaildata.bannertitle}</h2>
+              </div>
               <div className="row">
                 <div className="col-lg-7">
                   <div className="figurebg">
